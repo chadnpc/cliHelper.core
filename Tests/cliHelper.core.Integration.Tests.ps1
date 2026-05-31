16ff18919d4cf8628f9c2b888105ac5fc7b40b5a
alain <chadnpc@outlook.com>
2026-05-31 02:28:01 +0200

    Update cliHelper.core.Integration.Tests.ps1

--- Tests/cliHelper.core.Integration.Tests.ps1
+++ Tests/cliHelper.core.Integration.Tests.ps1
@@ -1,25 +1,25 @@
 Describe "Integration tests: cliHelper.core" {
   Context "Functionality Integration - Output Cmdlets" {
     It "Can output markup" {
-      { Write-Markup -MarkupText "[green]Test Markup[/]" } | Should -Not -Throw
+      { Write-Markup -MarkupText "[green]Test Markup[/]" } | Should Not Throw
     }
     It "Can output panels" {
-      { Write-Panel -Text "[red]Panel Content[/]" -Header "Test Header" } | Should -Not -Throw
+      { Write-Panel -Text "[red]Panel Content[/]" -Header "Test Header" } | Should Not Throw
     }
     It "Can output rules" {
-      { Write-Rule -Title "Rule Title" } | Should -Not -Throw
+      { Write-Rule -Title "Rule Title" } | Should Not Throw
     }
     It "Can output tables" {
-      { Write-Table -Columns @("Column A", "Column B") -Rows @(@("1", "2"), @("3", "4")) } | Should -Not -Throw
+      { Write-Table -Columns @("Column A", "Column B") -Rows @(@("1", "2"), @("3", "4")) } | Should Not Throw
     }
     It "Can output figlet text" {
-      { Write-Figlet -Text "Figlet Test" } | Should -Not -Throw
+      { Write-Figlet -Text "Figlet Test" } | Should Not Throw
     }
     It "Can output rows" {
-      { Write-Rows -Items @("[blue]Row 1[/]", "[yellow]Row 2[/]") } | Should -Not -Throw
+      { Write-Rows -Items @("[blue]Row 1[/]", "[yellow]Row 2[/]") } | Should Not Throw
     }
     It "Can output grids" {
-      { Write-Grid -Columns 2 -Rows @(@("Col1", "Col2"), @("Val1", "Val2")) } | Should -Not -Throw
+      { Write-Grid -Columns 2 -Rows @(@("Col1", "Col2"), @("Val1", "Val2")) } | Should Not Throw
     }
   }
 
@@ -31,7 +31,7 @@ Describe "Integration tests: cliHelper.core" {
           $task = $ctx.AddTask("Testing", [ProgressTaskSettings]::new())
           $task.Increment(100)
         }
-      } | Should -Not -Throw
+      } | Should Not Throw
     }
     It "Can show status" {
       {
@@ -39,13 +39,13 @@ Describe "Integration tests: cliHelper.core" {
           param([StatusContext]$ctx)
           $ctx.Update("Completed!")
         }
-      } | Should -Not -Throw
+      } | Should Not Throw
     }
   }
   Context "Functionality Integration - Runner module" {
     It "Can invoke retriable commands with Result class" {
       $Result = Invoke-RetriableCommand -ScriptBlock { return 0 } -MaxAttempts 1 -Message "Test Retriable"
-      $Result.IsSuccess | Should -Be $true
+      $Result.IsSuccess | Should Be $true
     }
   }
 }
